2eab52f66d9647661996e3e01e7c926491e8e6ec
Boopathy-senseai <boopathys@sense7ai.com>
2024-11-05 12:40:53 +0530

    file changing

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from statistics import fmean as mean
 from random import choices
 
+print("hai")
 data = [41, 50, 29, 37, 81, 30, 73, 63, 20, 35, 68, 22, 60, 31, 95]
 means = sorted(mean(choices(data, k=len(data))) for i in range(100))
 print(f'The sample mean of {mean(data):.1f} has a 90% confidence '
